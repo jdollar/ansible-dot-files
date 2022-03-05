8e53a605265e375ede404413e1dd4afd1dbd2087
Joel Dollarhide <dollarhidejoel@gmail.com>
2022-03-05 18:12:08 -0500

    convert neovim to lua config, primitive migration

---
+++ roles/neovim/files/init.lua
@@ -0,0 +1,50 @@
+HOME_DIR = os.getenv('HOME')
+
+vim.opt.filetype = 'off'
+
+vim.opt.compatible = false
+
+-- Set hybrid line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.background = 'dark'
+vim.opt.laststatus = 2
+vim.opt.wrap = false
+
+vim.opt.tags = './tags;' .. HOME_DIR
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.autoindent = true
+
+vim.call('plug#begin', HOME_DIR .. '/.config/nvim/bundle')
+
+local Plug = vim.fn['plug#']
+Plug('neoclide/coc.nvim', { branch = 'release' })
+Plug('bling/vim-airline')
+Plug('jeetsukumaran/vim-filebeagle')
+Plug('flazz/vim-colorschemes')
+Plug('w0rp/ale')
+Plug('rrrene/credo')
+Plug('airblade/vim-gitgutter')
+Plug('editorconfig/editorconfig-vim')
+Plug('scrooloose/nerdtree')
+Plug('junegunn/fzf', { dir = HOME_DIR .. '/.fzf', ['do'] = './install --all' })
+
+vim.call('plug#end')
+
+-- Disabling ale lsp so coc can do it
+vim.g.ale_disable_lsp = 1
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-b>',
+  ':NERDTreeToggle<CR>',
+  { noremap = false }
+)
+
