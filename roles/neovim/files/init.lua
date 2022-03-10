a93298caa8e31025abd7a906f1baecb570e06d34
Joel Dollarhide <dollarhidejoel@gmail.com>
2022-03-09 20:30:06 -0500

    adding fzf lua

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -49,11 +49,7 @@ Plug('junegunn/fzf.vim')
 
 vim.call('plug#end')
 
--- Require coc lua module
+-- Require lua modules for additional config
 require('coc')
-
--- Stops Rg from returning results based on file name matches
-vim.cmd([[
-command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)
-]])
+require('fzf')
 
---
+++ roles/neovim/files/lua/fzf/init.lua
@@ -0,0 +1,5 @@
+-- Stops Rg from returning results based on file name matches
+vim.cmd([[
+command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)
+]])
+
