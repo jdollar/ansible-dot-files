d0d40ec3f160a37c44840d184e622fbf343c809a
Joel Dollarhide <jdollarhide@botkeeper.com>
2022-03-14 16:55:44 -0400

    fix fzf

---
+++ roles/fzf/files/profile.d/fzf.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# Mac
+[ -f /usr/local/opt/fzf/shell/completion.zsh ] && source /usr/local/opt/fzf/shell/completion.zsh
+[ -f /usr/local/opt/fzf/shell/key-bindings.zsh ] && source /usr/local/opt/fzf/shell/key-bindings.zsh 
+
+# Linux
+[ -f /usr/share/fzf/shell/key-bindings.zsh ] && source /usr/share/fzf/shell/key-bindings.zsh
