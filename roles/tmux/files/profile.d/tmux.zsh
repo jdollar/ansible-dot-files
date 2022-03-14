328ee009298d59ddb854bc73170df1b8b43dfc57
Joel Dollarhide <jdollarhide@botkeeper.com>
2022-03-14 10:46:44 -0400

    fix issues with mac zprofile and fonts directory. Update asdf to include python version

--- roles/tmux/files/profile.d/tmux.zsh
+++ roles/tmux/files/profile.d/tmux.zsh
@@ -2,6 +2,6 @@
 
 #tmux
 if which tmux > /dev/null 2>&1; then
-  test -z "$TMUX" && (tmux attach || tmux new-session)
+  test -z "$TMUX" && (tmux attach -t default || tmux new-session -s default)
 fi
 
