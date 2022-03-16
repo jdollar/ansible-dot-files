71a26daa41d73ee1dc1cfcbe5f88522d26fd47a2
Joel Dollarhide <dollarhidejoel@gmail.com>
2022-03-15 20:53:15 -0400

    wrap for vscode

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,56 +1,57 @@
-HOME_DIR = os.getenv('HOME')
+if not vim.g.vscode then
+  HOME_DIR = os.getenv('HOME')
 
-vim.opt.filetype = 'off'
-vim.opt.mouse = 'a'
+  vim.opt.filetype = 'off'
+  vim.opt.mouse = 'a'
 
-vim.api.nvim_set_keymap(
-  'n',
-  '<Space>',
-  '<Nop>',
-  { noremap = false }
-)
+  vim.api.nvim_set_keymap(
+    'n',
+    '<Space>',
+    '<Nop>',
+    { noremap = false }
+  )
 
-vim.g.mapleader = ' '
+  vim.g.mapleader = ' '
 
-vim.opt.compatible = false
+  vim.opt.compatible = false
 
--- Set hybrid line numbering
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.nu = true
-vim.opt.rnu = true
+  -- Set hybrid line numbering
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+  vim.opt.nu = true
+  vim.opt.rnu = true
 
---vim.opt.background = 'dark'
-vim.opt.laststatus = 2
-vim.opt.wrap = false
+  --vim.opt.background = 'dark'
+  vim.opt.laststatus = 2
+  vim.opt.wrap = false
 
-vim.opt.tags = './tags;' .. HOME_DIR
+  vim.opt.tags = './tags;' .. HOME_DIR
 
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+  vim.opt.expandtab = true
+  vim.opt.tabstop = 2
+  vim.opt.shiftwidth = 2
 
-vim.opt.autoindent = true
+  vim.opt.autoindent = true
 
-vim.cmd([[
-command! PackerInstall packadd packer.nvim | lua require('plugins').install()
-command! PackerUpdate packadd packer.nvim | lua require('plugins').update()
-command! PackerSync packadd packer.nvim | lua require('plugins').sync()
-command! PackerClean packadd packer.nvim | lua require('plugins').clean()
-command! PackerStatus packadd packer.nvim | lua require('plugins').status()
-command! PackerCompile packadd packer.nvim | lua require('plugins').compile()
+  vim.cmd([[
+  command! PackerInstall packadd packer.nvim | lua require('plugins').install()
+  command! PackerUpdate packadd packer.nvim | lua require('plugins').update()
+  command! PackerSync packadd packer.nvim | lua require('plugins').sync()
+  command! PackerClean packadd packer.nvim | lua require('plugins').clean()
+  command! PackerStatus packadd packer.nvim | lua require('plugins').status()
+  command! PackerCompile packadd packer.nvim | lua require('plugins').compile()
 
-" Refresh packer lazy-loading when plugins.lua changes:
-augroup packer_user_config
-  autocmd!
-  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-augroup end
+  " Refresh packer lazy-loading when plugins.lua changes:
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
 
-" Install packer.nvim, if it isn't present:
-let g:install_path = stdpath('data') . '/site/pack/packer/opt/packer.nvim'
-if empty(glob(g:install_path))
-	call system(['git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', g:install_path])
-	PackerSync
-endif
-]])
-
+  " Install packer.nvim, if it isn't present:
+  let g:install_path = stdpath('data') . '/site/pack/packer/opt/packer.nvim'
+  if empty(glob(g:install_path))
+    call system(['git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', g:install_path])
+    PackerSync
+  endif
+  ]])
+end
