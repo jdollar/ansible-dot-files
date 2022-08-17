cf83e035d7e53938c4666f08b1b0c7a1dce483c0
Joel Dollarhide <dollarhidejoel@gmail.com>
2022-08-16 22:07:59 -0400

    move homebrew to mac script

--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -101,5 +101,19 @@ return require('packer').startup(function(use)
     'lukas-reineke/indent-blankline.nvim',
     config = function() require('indent-blankline-conf') end
   }
-end)
+
+  -- Display lsp errors better
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+    end
+  }
+end
+)
 
