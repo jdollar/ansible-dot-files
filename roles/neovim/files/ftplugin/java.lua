4a5d8bc27fb92e811ed9d1798e8528c28631b7c5
Joel Dollarhide <joel.dollarhide@appgate.com>
2026-01-22 15:16:35 -0500

    latest work updates

--- roles/neovim/files/ftplugin/java.lua
+++ roles/neovim/files/ftplugin/java.lua
@@ -1,42 +1,42 @@
 local config = {
   cmd = { vim.fn.stdpath('data') .. '/mason/bin/jdtls' },
   root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-  settings = {
-    java = {
-      format = {
-        enabled = false,
-      },
-      imports = {
-        gradle = {
-          enabled = true,
-          wrapper = {
-            enabled = true,
-            checksums = {
-              { sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172", allowed = true }
-            }
-          }
-        }
-      }
-    }
-  },
-  init_options = {
-    bundle = {},
-    settings = {
-      java = {
-        imports = {
-          gradle = {
-            enabled = true,
-            wrapper = {
-              enabled = true,
-              checksums = {
-                { sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172", allowed = true }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+--  settings = {
+--    java = {
+--      format = {
+--        enabled = false,
+--      },
+--      imports = {
+--        gradle = {
+--          enabled = true,
+--          wrapper = {
+--            enabled = true,
+--            checksums = {
+--              { sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172", allowed = true }
+--            }
+--          }
+--        }
+--      }
+--    }
+--  },
+--  init_options = {
+--    bundle = {},
+--    settings = {
+--      java = {
+--        imports = {
+--          gradle = {
+--            enabled = true,
+--            wrapper = {
+--              enabled = true,
+--              checksums = {
+--                { sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172", allowed = true }
+--              }
+--            }
+--          }
+--        }
+--      }
+--    }
+--  }
 }
 
 require('jdtls').start_or_attach(config)
--- roles/neovim/files/lua/plugins/editorconfig-vim.lua
+++
@@ -1,7 +0,0 @@
--- enables use of .editorconfig files
-return {
-  'editorconfig/editorconfig-vim',
-  config = function()
-    vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*' }
-  end
-}
