42198e62b70f9f5f97275d0227603d92b4e533be
Joel Dollarhide <dollarhidejoel@gmail.com>
2026-02-14 10:08:27 -0500

    move to asdf-plugin-manager

--- roles/asdf/files/env.d/asdf.zsh
+++ roles/asdf/files/env.d/asdf.zsh
@@ -3,6 +3,7 @@
 # Reads go version from go mod and go work files
 export ASDF_DATA_DIR=$HOME/.asdf
 export ASDF_GOLANG_MOD_VERSION_ENABLED=true
+export ASDF_PLUGIN_MANAGER_PLUGIN_VERSIONS_FILENAME=~/.plugin-versions
 export PATH=$ASDF_DATA_DIR/bin:$PATH
 export PATH=$ASDF_DATA_DIR/shims:$PATH
 export PATH=~/.local/bin:$PATH
