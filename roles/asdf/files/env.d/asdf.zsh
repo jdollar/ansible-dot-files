8b7b08a505ab3fc342745f6d5e1034d09f495b46
Joel Dollarhide <joel.dollarhide@appgate.com>
2026-02-20 16:39:06 -0500

    setup nexus

--- roles/asdf/files/env.d/asdf.zsh
+++ roles/asdf/files/env.d/asdf.zsh
@@ -2,10 +2,20 @@
 
 # Reads go version from go mod and go work files
 export ASDF_DATA_DIR=$HOME/.asdf
-export ASDF_GOLANG_MOD_VERSION_ENABLED=true
 export ASDF_PLUGIN_MANAGER_PLUGIN_VERSIONS_FILENAME=~/.plugin-versions
 export PATH=$ASDF_DATA_DIR/bin:$PATH
 export PATH=$ASDF_DATA_DIR/shims:$PATH
-export PATH=~/.local/bin:$PATH
-source ${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/golang/set-env.zsh
-#source ${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/java/set-java-home.zsh
+export PATH=$HOME/.local/bin:$PATH
+
+# Java
+java_plugin_set_script="${ASDF_DATA_DIR}"/plugins/java/set-java-home.zsh
+if [[ -n "${java_plugin_set_script}" ]]; then
+  source "${java_plugin_set_script}"
+fi
+
+# Go
+export ASDF_GOLANG_MOD_VERSION_ENABLED=true
+go_plugin_set_script="${ASDF_DATA_DIR}"/plugins/golang/set-env.zsh
+if [[ -n "${go_plugin_set_script}" ]]; then
+  source "${go_plugin_set_script}"
+fi
